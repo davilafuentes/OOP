cf038097d579396622f16270d6adf90a3f04880b
David Ávila Fuentes <david.avila.fuentes@gmail.com>
2021-05-20 20:53:48 -0600

    Added the Hello Class line.

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,8 +1,11 @@
 package com.ucreativa;
 
+import com.ucreativa.familia.davilafuentes;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Class!");
+
+        davilafuentes david = new davilafuentes();
     }
 }
---
+++ src/com/ucreativa/familia/davilafuentes.java
@@ -0,0 +1,8 @@
+package com.ucreativa.familia;
+
+public class davilafuentes {
+
+    public davilafuentes(){
+        System.out.println("Hola, yo soy David!");
+    }
+}
